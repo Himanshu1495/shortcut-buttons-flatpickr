1fed7cbc65ca692509d3e6a2b0b4f08dfb63bc07
Himanshu Nachane <himanshu.n1495@gmail.com>
2019-09-12 21:20:24 +0530

    dist added

---
+++ dist/types/index.d.ts
@@ -0,0 +1,22 @@
+import { Instance as Flatpickr } from 'flatpickr/dist/types/instance.d';
+export declare namespace ShortcutButtonsFlatpickr {
+    type OnClickSignature = (index: number, fp: Flatpickr) => void;
+    type Attributes = {
+        [name: string]: string;
+    };
+    type Button = {
+        attributes?: Attributes;
+        label: string;
+    };
+    type Config = {
+        button: Button | Button[];
+        label?: string;
+        onClick?: OnClickSignature | OnClickSignature[];
+        theme?: string;
+        expand?: boolean;
+    };
+}
+export declare function ShortcutButtonsPlugin(config: ShortcutButtonsFlatpickr.Config): (fp: Flatpickr) => {
+    onReady: () => void;
+    onDestroy: () => void;
+};
